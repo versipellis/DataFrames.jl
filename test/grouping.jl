JuliaData/DataTables.jl

Update combine to use splat `...` style vcat, like in Base.vcat
This change is helpful to support changing vcat to be more consistent
with Base.vcat, where passing an array is not allowed.

test/grouping.jl
@@ -146,7 +146,7 @@ module TestGrouping
     groupby(dt, [:v1, :v2])
 
     dt2 = by(e->1, DataTable(x=Int64[]), :x)
-    @test size(dt2) == (0,1)
+    @test size(dt2) == (0,2)
     @test isequal(sum(dt2[:x]), Nullable(0))
 
     # Check that reordering levels does not confuse groupby

versipellis/DataFrames.jl

test/grouping.jl
@@ -146,7 +146,7 @@ module TestGrouping
     groupby(df, [:v1, :v2])
 
     df2 = by(e->1, DataFrame(x=Int64[]), :x)
-    @test size(df2) == (0,1)
+    @test size(df2) == (0,2)
     @test isequal(sum(df2[:x]), Nullable(0))
 
     # Check that reordering levels does not confuse groupby